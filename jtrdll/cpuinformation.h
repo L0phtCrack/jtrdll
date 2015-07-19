905db26cc4c83a24351a92eefde9409e1207914f
Christien Rioux <crioux@noctem.org>
2015-07-18 18:38:25 -0700

    add jtrexe back in with cpu detection

---
+++ jtrdll/cpuinformation.h
@@ -0,0 +1,106 @@
+#ifndef __INC_CPUINFORMATION
+#define __INC_CPUINFORMATION
+
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <array>
+#include <string>
+
+class CPUInformation
+{
+	// forward declarations
+	class InstructionSet_Internal;
+
+public:
+
+	// getters
+	virtual int CoreCount(void);
+	virtual QString Vendor(void);
+	virtual QString Brand(void);
+
+	virtual bool SSE3(void);
+	virtual bool PCLMULQDQ(void);
+	virtual bool MONITOR(void);
+	virtual bool SSSE3(void);
+	virtual bool FMA(void);
+	virtual bool CMPXCHG16B(void);
+	virtual bool SSE41(void);
+	virtual bool SSE42(void);
+	virtual bool MOVBE(void);
+	virtual bool POPCNT(void);
+	virtual bool AES(void);
+	virtual bool XSAVE(void);
+	virtual bool OSXSAVE(void);
+	virtual bool AVX(void);
+	virtual bool F16C(void);
+	virtual bool RDRAND(void);
+
+	virtual bool MSR(void);
+	virtual bool CX8(void);
+	virtual bool SEP(void);
+	virtual bool CMOV(void);
+	virtual bool CLFSH(void);
+	virtual bool MMX(void);
+	virtual bool FXSR(void);
+	virtual bool SSE(void);
+	virtual bool SSE2(void);
+
+	virtual bool FSGSBASE(void);
+	virtual bool BMI1(void);
+	virtual bool HLE(void);
+	virtual bool AVX2(void);
+	virtual bool BMI2(void);
+	virtual bool ERMS(void);
+	virtual bool INVPCID(void);
+	virtual bool RTM(void);
+	virtual bool AVX512F(void);
+	virtual bool RDSEED(void);
+	virtual bool ADX(void);
+	virtual bool AVX512PF(void);
+	virtual bool AVX512ER(void);
+	virtual bool AVX512CD(void);
+	virtual bool SHA(void);
+
+	virtual bool PREFETCHWT1(void);
+
+	virtual bool LAHF(void);
+	virtual bool LZCNT(void);
+	virtual bool ABM(void);
+	virtual bool SSE4a(void);
+	virtual bool XOP(void);
+	virtual bool TBM(void);
+
+	virtual bool SYSCALL(void);
+	virtual bool MMXEXT(void);
+	virtual bool RDTSCP(void);
+	virtual bool _3DNOWEXT(void);
+	virtual bool _3DNOW(void);
+
+private:
+
+	class InstructionSet_Internal
+	{
+	public:
+		InstructionSet_Internal();
+
+		int nIds_;
+		int nExIds_;
+		std::string vendor_;
+		std::string brand_;
+		bool isIntel_;
+		bool isAMD_;
+		std::bitset<32> f_1_ECX_;
+		std::bitset<32> f_1_EDX_;
+		std::bitset<32> f_7_EBX_;
+		std::bitset<32> f_7_ECX_;
+		std::bitset<32> f_81_ECX_;
+		std::bitset<32> f_81_EDX_;
+		std::vector<std::array<int, 4>> data_;
+		std::vector<std::array<int, 4>> extdata_;
+	};
+
+	InstructionSet_Internal CPU_Rep;
+};
+
+#endif
---
+++ jtrdll/jtrexe.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#ifdef _WIN32
+#include<windows.h>
+#endif
+#include"cpuinformation.h"
+
+
+
+
+
+int (*TYPEOF_jtrdll_main)(int argc, char **argv, void *hooks);
+
+int main(int argc, char **argv)
+{
+	CPUInformation cpu;
+	std::string jtrdllversion;
+	
+	jtrdllversion  = "sse2";
+	if (cpu.SSSE3())
+	{
+		jtrdllversion = "ssse3";
+		if (cpu.SSE41())
+		{
+			jtrdllversion = "sse41";
+			
+			if (cpu.XOP())
+			{
+				jtrdllversion = "xop";
+			}
+			else if (cpu.AVX())
+			{
+				jtrdllversion = "avx";
+			}
+		}
+	}
+	
+#ifdef _WIN32
+	HMODULE jtrdll = LoadLibrary("jtrdll_"+jtrdllversion);
+	if(!jtrdll)
+	{
+		fprintf(stderr,"Missing jtrdll\n");
+		exit(1);
+	}
+	TYPEOF_jtrdll_main jtrdll_main = (TYPEOF_jtrdll_main) GetProcAddress(jtrdll,"jtrdll_main");
+	if(!jtrdll_main)
+	{
+		fprintf(stderr,"Malformed jtrdll\n");
+		FreeLibrary(jtrdll);
+		exit(1);
+	}
+#else
+#error "to do"
+#endif
+	
+	int ret = jtrdll_main(argc,argv,NULL);
+	
+#ifdef _WIN32
+	FreeLibrary(jtrdll);
+#else
+#error "to do"
+#endif
+
+	return ret;
+}
